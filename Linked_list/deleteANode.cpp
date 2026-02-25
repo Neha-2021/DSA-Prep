23538de216524f3e467b45c048f57768fc276d55
Neha-2021 <nehakumari01012001@gmail.com>
2026-02-25 22:08:21 +0530

    Practice: delete a given node of the linked list

---
+++ Linked_list/deleteANode.cpp
@@ -0,0 +1,73 @@
+/*
+Problem Statement: Given a linked list and an integer value val, insert a new node with that value at the beginning (before the head) of the list 
+and return the updated linked list.
+
+Input: 0->1->2, val = 5 
+Output: 5->0->1->2
+
+
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct LinkedList 
+{
+    public:
+    int data;
+    LinkedList* next;
+
+    public:
+    LinkedList(int data1, LinkedList* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    LinkedList(int data1) {
+        data = data1;
+    }
+};
+
+LinkedList* convertArrayToLinkedList(vector<int> &arr) {
+    LinkedList* head = new LinkedList(arr[0]);
+    LinkedList* mover = head;
+
+    for(int i=1; i<arr.size(); i++) {
+        LinkedList* temp = new LinkedList(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+    return head;
+}
+
+void deleteNode(LinkedList* node) {
+    LinkedList* nextNode = node->next;
+    node->data = node->next->data;
+    node->next = node->next->next;
+    delete nextNode;
+}
+
+void printNode(LinkedList* head) {
+    LinkedList* temp = head;
+    while(temp) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << "\n";
+}
+
+int main() {
+    vector<int> arr = {2, 3, 4, 5, 6};
+    LinkedList* head = convertArrayToLinkedList(arr);
+    cout << "Array is converted to LL, head = " << head->data << "\n"; 
+    cout << "LinkedList before deletion: \n";
+    printNode(head);
+
+    LinkedList* nodeToDelete = head->next->next;
+    cout << "Node to delete = " << nodeToDelete->data << "\n";
+    deleteNode(head->next->next);
+    cout << "LinkedList after deletion: \n";
+    printNode(head);
+}
